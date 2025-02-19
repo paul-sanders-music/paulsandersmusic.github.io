aad9d6a1b9a5472c33b5a961b63fc5fbcbc4cf26
Paul Sanders <paul@paulsanders.dev>
2025-02-19 11:44:59 -0500

    Make contact form functional

---
+++ tests/contact_form.spec.ts
@@ -0,0 +1,24 @@
+import { type Page, expect, test } from "@playwright/test";
+
+interface OptionsType {
+  exact?: boolean;
+}
+
+const verifyInput = async (
+  page: Page,
+  placeholder: string,
+  options?: OptionsType,
+) => {
+  const input = page.getByPlaceholder(placeholder, options);
+  await expect(input).toBeVisible();
+  await expect(input).toHaveText("");
+  await expect(input).toBeEditable();
+};
+
+test("Inputs are visible, empty and editable", async ({ page }) => {
+  await page.goto("/");
+
+  await verifyInput(page, "Name", { exact: true });
+  await verifyInput(page, "Email Address", { exact: true });
+  await verifyInput(page, "Your Message", { exact: true });
+});
